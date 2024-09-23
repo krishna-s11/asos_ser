87384af00a45fde650b5f53bb69b6791578869ef
krishna-s11 <krishnasaxena69@gmail.com>
2024-09-23 08:06:13 +0530

    port change

--- server.js
+++ server.js
@@ -301,6 +301,7 @@ app.post('/api/upload_file', upload.single('file'), (req, res) => {
 
   });
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-})
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+      console.log('Server is running on port 8080');
+  })
